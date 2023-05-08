bc5ee3ca9737ee8e0d417234d0c3728086e534c8
Nino Filiu <nino.filiu@gmail.com>
2023-05-08 22:12:52 +0200

    postprocessing

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useFrame } from "@react-three/fiber";
 import { useRef } from "react";
 import type { Mesh } from "three";
 import { x } from "./shorts";
+import { Bloom, EffectComposer } from "@react-three/postprocessing";
 
 export const App = () => {
   const mesh = useRef<Mesh>(null);
@@ -14,8 +15,11 @@ export const App = () => {
       <pointLight position={[10, 10, 10]} />
       <mesh ref={mesh}>
         <boxGeometry args={[1, 1, 1]} />
-        <meshStandardMaterial color="red" />
+        <meshStandardMaterial color="white" wireframe />
       </mesh>
+      <EffectComposer>
+        <Bloom luminanceThreshold={0.2} />
+      </EffectComposer>
     </>
   );
 };
