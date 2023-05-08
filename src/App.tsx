75dcfa3fb321c082f6bbd767968acbd320f59a33
Nino Filiu <nino.filiu@gmail.com>
2023-05-09 01:36:39 +0200

    point at raycast objects

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { useFrame } from "@react-three/fiber";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { Bloom, EffectComposer } from "@react-three/postprocessing";
 import { Environment, PointerLockControls } from "@react-three/drei";
+import type { Mesh, Raycaster } from "three";
+import { Vector2 } from "three";
 import { Vector3 } from "three";
 import type { PointerLockControls as PointerLockControlsType } from "three-stdlib/controls/PointerLockControls";
-import { KernelSize } from "postprocessing";
 
 const SPEED = 0.05;
 
@@ -14,9 +15,12 @@ document.addEventListener("keyup", (evt) => (pressed[evt.code] = false));
 
 export const App = () => {
   const controls = useRef<PointerLockControlsType>(null);
+  const raycaster = useRef<Raycaster>(null);
+  const box = useRef<Mesh>(null);
+  const [pointed, setPointed] = useState(false);
 
   useFrame(() => {
-    if (!controls.current) return;
+    if (!controls.current || !raycaster.current || !box.current) return;
     const d = new Vector3(0, 0, 0);
     if (pressed.KeyQ) d.y += SPEED;
     if (pressed.KeyW) d.z -= SPEED;
@@ -26,34 +30,29 @@ export const App = () => {
     if (pressed.KeyA) d.x -= SPEED;
     d.applyEuler(controls.current.camera.rotation);
     controls.current.camera.position.add(d);
+
+    raycaster.current.setFromCamera(new Vector2(0, 0), controls.current.camera);
+    setPointed(!!raycaster.current.intersectObject(box.current).length);
   });
 
   return (
     <>
-      <mesh position={[0, 0, 0]}>
-        <sphereGeometry args={[0.5, 16, 32]} />
-        <meshStandardMaterial roughness={0} metalness={1} />
-      </mesh>
-
-      <mesh>
-        <boxGeometry args={[20, 20, 20, 20, 20, 20]} />
-        <meshBasicMaterial wireframe color={"lime"} />
+      <mesh ref={box}>
+        <boxGeometry args={[1, 1, 1]} />
+        <meshStandardMaterial color={pointed ? "red" : "white"} />
       </mesh>
 
       <axesHelper />
 
       <ambientLight />
       <hemisphereLight />
-      <Environment preset="dawn" background />
+      <Environment preset="dawn" />
 
       <PointerLockControls pointerSpeed={1} ref={controls} />
+      <raycaster ref={raycaster} />
 
       <EffectComposer>
-        <Bloom
-          luminanceThreshold={0.9}
-          intensity={0.2}
-          kernelSize={KernelSize.LARGE}
-        />
+        <Bloom luminanceThreshold={0.2} />
       </EffectComposer>
     </>
   );
